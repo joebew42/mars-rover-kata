037c5a702ec0337ae14ddae74bd15d3bb07b8f58
Joe Bew <joebew42@gmail.com>
2020-08-29 21:00:53 +0200

    A rover has an initial direction

--- src/main/java/org/kata/Rover.java
+++ src/main/java/org/kata/Rover.java
@@ -4,9 +4,14 @@ import java.util.Objects;
 
 public class Rover {
     private Point point;
+    private Direction direction;
 
     public Rover(Point point) {
         this.point = point;
+    }
+
+    public Rover(Direction direction) {
+        this.direction = direction;
     }
 
     @Override
@@ -14,11 +19,17 @@ public class Rover {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Rover rover = (Rover) o;
-        return Objects.equals(point, rover.point);
+        return Objects.equals(point, rover.point) &&
+                direction == rover.direction;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(point);
+        return Objects.hash(point, direction);
+    }
+
+    public enum Direction {
+        North,
+        South;
     }
 }
--- src/test/java/org/kata/RoverTest.java
+++ src/test/java/org/kata/RoverTest.java
@@ -3,7 +3,9 @@ package org.kata;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
+import static org.kata.Rover.Direction.North;
+import static org.kata.Rover.Direction.South;
 
 public class RoverTest {
     @Test
@@ -17,6 +19,15 @@ public class RoverTest {
 
     @Test
     public void a_rover_has_a_direction() {
-        assertTrue(false);
+        Rover rover = new Rover(North);
+
+        assertEquals(new Rover(North), rover);
+    }
+
+    @Test
+    public void two_rovers_with_different_directions_are_not_equals() {
+        Rover rover = new Rover(South);
+
+        assertNotEquals(new Rover(North), rover);
     }
 }
