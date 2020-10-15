2cf97cfefa4f8da8f537cca0b860e9ddaada766f
Joe Bew <joebew42@gmail.com>
2020-10-15 19:16:28 +0200

    Re-arrange methods

--- src/main/java/org/kata/marsrover/Rover.java
+++ src/main/java/org/kata/marsrover/Rover.java
@@ -21,14 +21,14 @@ public class Rover {
         this.commands = commands;
     }
 
-    public void execute(char commandCharacter) {
-        findCommandBy(commandCharacter).apply(this);
-    }
-
     public void execute(char[] arrayOfCommands) {
         for (char command : arrayOfCommands) {
             execute(command);
         }
+    }
+
+    public void execute(char commandCharacter) {
+        findCommandBy(commandCharacter).apply(this);
     }
 
     public void rotateLeft() {
